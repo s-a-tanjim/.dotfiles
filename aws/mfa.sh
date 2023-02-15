5767f0abe4f297f680d76f3ec9ddbc7e85d73eff
Shoeb Ahmed Tanjim <39959228+s-a-tanjim@users.noreply.github.com>
2023-02-15 15:23:07 +0600

    Update mfa.sh

--- aws/mfa.sh
+++ aws/mfa.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 
+# To Run(Including source variable): `$ . ./connect-mfa.sh`
+
+
 # Dependencies
 # - jq
 
-ARN_VALUE=''
+ARN_VALUE=arn:aws:iam::*:mfa/*
 
 echo Enter Auth Code:
 read TOKEN_CODE
@@ -13,11 +16,21 @@ read -r -d '' OUTPUT << EOM
 $(aws sts get-session-token --serial-number $ARN_VALUE --token-code $TOKEN_CODE)
 EOM
 
-ACCESS_KEY_ID=$(echo $OUTPUT | jq .Credentials.AccessKeyId)
-SECRET_ACCESS_KEY=$(echo $OUTPUT | jq .Credentials.SecretAccessKey)
-SESSION_TOKEN=$(echo $OUTPUT | jq .Credentials.SessionToken)
 
+export AWS_ACCESS_KEY_ID=$(echo $OUTPUT | jq .Credentials.AccessKeyId)
+export AWS_SECRET_ACCESS_KEY=$(echo $OUTPUT | jq .Credentials.SecretAccessKey)
+export AWS_SESSION_TOKEN=$(echo $OUTPUT | jq .Credentials.SessionToken)
+export AWS_TOKEN_EXPIRATION=$(echo $OUTPUT | jq .Credentials.Expiration)
 
-export $ACCESS_KEY_ID
-export $SECRET_ACCESS_KEY
-export $SESSION_TOKEN
+echo '\nAWSAccessKeyID: '
+echo $AWS_ACCESS_KEY_ID
+echo '\nAWSSecretAccessKey: '
+echo $AWS_SECRET_ACCESS_KEY
+echo '\nAWSSessionToken: '
+echo $AWS_SESSION_TOKEN
+echo '\nAWSTokenExpiration: '
+echo $AWS_TOKEN_EXPIRATION
+
+# export $ACCESS_KEY_ID
+# export $SECRET_ACCESS_KEY
+# export $SESSION_TOKEN
