6240a363a35f870fde158d7dad4cef48bc033322
Shoeb Ahmed Tanjim <39959228+s-a-tanjim@users.noreply.github.com>
2023-02-16 12:14:23 +0600

    Update mfa.sh

--- aws/mfa.sh
+++ aws/mfa.sh
@@ -1,12 +1,21 @@
 #!/bin/bash
 
+#################################################
+##       AWS Profile Set For MFA on CLI        ##
+#################################################
+
+
 # To Run(Including source variable): `$ . ./connect-mfa.sh`
 
 
 # Dependencies
 # - jq
 
+
+####   SET ENV VARIABLES!   ####
 ARN_VALUE=arn:aws:iam::*:mfa/*
+AWS_REGION=ap-southeast-1
+PROFILE=mfa-profile
 
 echo Enter Auth Code:
 read TOKEN_CODE
@@ -17,20 +26,25 @@ $(aws sts get-session-token --serial-number $ARN_VALUE --token-code $TOKEN_CODE)
 EOM
 
 
-export AWS_ACCESS_KEY_ID=$(echo $OUTPUT | jq .Credentials.AccessKeyId)
-export AWS_SECRET_ACCESS_KEY=$(echo $OUTPUT | jq .Credentials.SecretAccessKey)
-export AWS_SESSION_TOKEN=$(echo $OUTPUT | jq .Credentials.SessionToken)
-export AWS_TOKEN_EXPIRATION=$(echo $OUTPUT | jq .Credentials.Expiration)
+export AWS_ACCESS_KEY_ID=$(echo $OUTPUT | jq --raw-output .Credentials.AccessKeyId)
+export AWS_SECRET_ACCESS_KEY=$(echo $OUTPUT | jq --raw-output .Credentials.SecretAccessKey)
+export AWS_SESSION_TOKEN=$(echo $OUTPUT | jq --raw-output .Credentials.SessionToken)
+export AWS_TOKEN_EXPIRATION=$(echo $OUTPUT | jq --raw-output .Credentials.Expiration)
 
-echo '\nAWSAccessKeyID: '
+echo -e '\nAWSAccessKeyID: '
 echo $AWS_ACCESS_KEY_ID
-echo '\nAWSSecretAccessKey: '
+echo -e '\nAWSSecretAccessKey: '
 echo $AWS_SECRET_ACCESS_KEY
-echo '\nAWSSessionToken: '
+echo -e '\nAWSSessionToken: '
 echo $AWS_SESSION_TOKEN
-echo '\nAWSTokenExpiration: '
+echo -e '\nAWSTokenExpiration: '
 echo $AWS_TOKEN_EXPIRATION
 
-# export $ACCESS_KEY_ID
-# export $SECRET_ACCESS_KEY
-# export $SESSION_TOKEN
+aws configure set region $AWS_REGION --profile $PROFILE
+aws configure set aws_access_key_id $AWS_ACCESS_KEY_ID --profile $PROFILE
+aws configure set aws_secret_access_key $AWS_SECRET_ACCESS_KEY --profile $PROFILE
+aws configure set aws_session_token $AWS_SESSION_TOKEN --profile $PROFILE
+
+echo 'AWS Profile:'
+aws configure list-profiles
+
